5a3a90f4e7737ebcc8106c1d52bfb9f48a9e3697
Satya Prakash Jha <spjha@modeln.com>
2021-12-11 00:24:56 +0530

    update color scheme for charts

--- src/pages/Pie/index.js
+++ src/pages/Pie/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
+// import * as brewer from 'chartjs-plugin-colorschemes/src/colorschemes/colorschemes.brewer';
+import * as office from 'chartjs-plugin-colorschemes/src/colorschemes/colorschemes.office';
+// import * as tableau from 'chartjs-plugin-colorschemes/src/colorschemes/colorschemes.tableau';
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
@@ -10,20 +14,8 @@ export const data = {
   datasets: [
     {
       label: 'Rainfall',
-      backgroundColor: [
-        '#B21F00',
-        '#C9DE00',
-        '#2FDE00',
-        '#00A6B4',
-        '#6800B4'
-      ],
-      hoverBackgroundColor: [
-        '#501800',
-        '#4B5000',
-        '#175000',
-        '#003350',
-        '#35014F'
-      ],
+      // Ref: https://nagix.github.io/chartjs-plugin-colorschemes/colorchart.html
+      backgroundColor: office.BlueGreen6,
       data: [65, 59, 80, 81, 56]
     }
   ]
