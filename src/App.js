ebbffe7e69ca019d73e3bec9f638ba51ebe127af
Satya Prakash Jha <spjha@modeln.com>
2021-12-11 02:42:12 +0530

    add logged in info and logout button
    Signed-off-by: Satya Prakash Jha <spjha@modeln.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
@@ -7,7 +7,7 @@ import useAuthToken from './useAuthToken';
 import LoginForm from './pages/LoginForm';
 
 const { Header, Footer, Content } = Layout;
-
+/*
 function MainContent() {
   const { token, setToken } = useAuthToken();
 
@@ -24,15 +24,29 @@ function MainContent() {
     </BrowserRouter>
   </div>
 }
-
+*/
 function App() {
+  const { token, setToken } = useAuthToken();
+
   return (
     <div className="app">
       <Layout>
-        <Header>Model N Co-Pilot</Header>
+        <Header>
+          { (token) ? (<div style={{float:'right'}}>You are logged in as {token.username}. <Button type='text' style={{color: 'white'}} onClick={e => setToken(null)}>logout</Button></div>): ''}
+          <div style={{fontWeight: 'bold', fontSize: 'large'}}>Model N Co-Pilot</div>
+        </Header>
         <Content className="site-layout-content" style={{ padding: '60px 50px', paddingTop: 0 }}>
           <Layout style={{ padding: 50, paddingTop: 100 }}>
-            <MainContent />
+            {
+              (!token) ? (<div style={{ paddingTop: 60 }}><LoginForm setToken={setToken} /></div>) : (<div style={{ backgroundColor: '#fff', padding: 30 }}>
+                <BrowserRouter>
+                  <Routes>
+                    <Route path="/" element={<Container />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                  </Routes>
+                </BrowserRouter>
+              </div>)
+            }
           </Layout>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Model N Co-Pilot &copy; 2021</Footer>
